bf7a8f2c15b27eb7baee7e0f7244e173069064ee
Diego-Diego <diego.git.friends@gmail.com>
2022-11-13 19:51:29 +0100

    1.0

---
+++ resources/[modding]/[mapping]/mapping_paris/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'xml/*.xml'
+
+files {
+	'xml/*.xml',
+}
+
+client_script {
+    "lua/*.lua"
+}
+
+--mcdonalds
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/ch_dlc_int_02_ch.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/mcd2'
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/ch_prop_ch_casino_backarea.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/ch_prop_ch_casino_main.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/ch_prop_ch_casino_vault.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/h4_dlc_int_05_h4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mcdonalds/h4_prop_h4_island_02.ytyp'
